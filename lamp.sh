cd1b42a4273504b127904b181d5882aeae7f8fe5
skulldouggery <dougddo@gmail.com>
2014-05-19 22:15:26 -0700

    Update lamp.sh

--- lamp.sh
+++ lamp.sh
@@ -58,6 +58,7 @@ mysql -uroot -e "CREATE DATABASE ${DB_NAME}" -p$DB_PASSWORD
 #READ: This is a temporary fix for problems with mysql connections while using a vhost setup
 sudo sed -i "s/127.0.0.1/0.0.0.0/g" /etc/mysql/my.cnf
 echo "GRANT ALL PRIVILEGES ON *.* TO 'root'@'$HOST_NAME' IDENTIFIED BY '$DB_PASSWORD' WITH GRANT OPTION;"| mysql -uroot -p$DB_PASSWORD
+echo "FLUSH PRIVILEGES"| mysql -uroot -p$DB_PASSWORD
 sudo service mysql restart
 
 echo "Installing PHP."
