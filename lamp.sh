152eed32107dfcd672f769d20b398e4999d75d91
skulldouggery <dougddo@gmail.com>
2014-05-19 18:16:36 -0700

    Update lamp.sh

--- lamp.sh
+++ lamp.sh
@@ -55,6 +55,13 @@ sudo service mysql restart
 
 mysql -uroot -e "create database ${DB_NAME}" -p$DB_PASSWORD
 
+#READ: This is a temporary fix for problems with mysql connections while using a vhost setup
+sed -i "s/bind-address = 127.0.0.1/bind-address = 0.0.0.0/g" /etc/mysql/my.cnf
+mysql -uroot -p$DB_PASSOWORD
+echo "GRANT ALL PRIVILEGES ON *.* TO 'root'@'${HOST_NAME}' IDENTIFIED BY '${DB_PASSWORD}' WITH GRANT OPTION;"| mysql
+echo "exit"| mysql
+sudo service mysql restart
+
 echo "Installing PHP."
 sudo add-apt-repository -y ppa:ondrej/php5
 sudo apt-get update
